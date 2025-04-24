e33875b9e993978e42e0f2314c018d52cb8ba465
Mateo Palmeiro Muñiz <mateo.palmeiro@udc.es>
2025-04-24 20:57:39 +0200

    types estatico

--- Plantilla-Template_P2/types.h
+++ Plantilla-Template_P2/types.h
@@ -9,7 +9,8 @@ AUTHOR 2: Nicolas Otero Costa            LOGIN 2: nicolas.otero1@udc.es
 GROUP: 4.3    DATE: 04/04/2025
 */
 
-#define NAME_LENGTH_LIMIT 10  // longitud máxima de identificadores
+#define NAME_LENGTH_LIMIT 10  // longitud maxima de identificadores
+#define BID_STACK_SIZE    25  // tamano maximo de la pila de pujas
 
 typedef char tUserId[NAME_LENGTH_LIMIT];
 typedef char tConsoleId[NAME_LENGTH_LIMIT];
@@ -17,9 +18,29 @@ typedef char tConsoleId[NAME_LENGTH_LIMIT];
 typedef enum { nintendo, sega } tConsoleBrand;
 
 typedef float tConsolePrice;
-typedef int tBidCounter;
+typedef int   tBidCounter;
 
-typedef struct sStack tStack;
+/*
+ * tItemS: estructura que almacena los datos de una puja.
+ * Compuesto por:
+ *   - bidder: tUserId
+ *   - consolePrice: tConsolePrice
+ */
+typedef struct {
+    tUserId       bidder;       // id del pujador
+    tConsolePrice consolePrice; // importe de la puja
+} tItemS;
+
+/*
+ * tStack: pila estatica de pujas.
+ * Compuesto por:
+ *   - items: array de tItemS de tamano BID_STACK_SIZE
+ *   - top: indice del elemento en la cima (-1 si vacia)
+ */
+typedef struct sStack {
+    tItemS items[BID_STACK_SIZE];
+    int    top;  // indice de la cima
+} tStack;
 
 /*
  * tItemL: estructura que almacena los datos de una consola.
@@ -31,14 +52,13 @@ typedef struct sStack tStack;
  *   - bidCounter: tBidCounter
  *   - bidStack: tStack (pila de pujas asociada a la consola)
  */
-typedef struct tItemL {
-    tUserId seller;
-    tConsoleId consoleId;
-    tConsoleBrand consoleBrand;
-    tConsolePrice consolePrice;
-    tBidCounter bidCounter;
-    tStack *bidStack;
+typedef struct {
+    tUserId        seller;       // id del vendedor
+    tConsoleId     consoleId;    // id de la consola
+    tConsoleBrand  consoleBrand; // marca de la consola
+    tConsolePrice  consolePrice; // precio original
+    tBidCounter    bidCounter;   // numero de pujas recibidas
+    tStack         bidStack;     // pila de pujas asociada
 } tItemL;
 
-#endif
-
+#endif  // TYPES_H
